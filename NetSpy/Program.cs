666a608ea978dacebf363bc4eb6a796a6259c1dd
xafero <georg.berendt@uni-oldenburg.de>
2017-09-18 21:09:39 +0200

    Added initial stuff

---
+++ NetSpy/Program.cs
@@ -0,0 +1,13 @@
+﻿using ICSharpCode.Decompiler;
+
+namespace NetSpy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new DecompilerSettings();
+
+        }
+    }
+}
